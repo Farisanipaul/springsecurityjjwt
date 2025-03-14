673d3630639525f1109f4b61bcc3f779831db37f
Farisani-Ratshikombo <ratshikombofarisani@gmail.com>
2025-03-14 10:39:48 +0200

    adding docker support and build for the project.

--- src/test/java/com/springbootsecurityjwt/springsecurityjjwt/SpringsecurityjjwtApplicationTests.java
+++ src/test/java/com/springbootsecurityjwt/springsecurityjjwt/SpringsecurityjjwtApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringsecurityjjwtApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
