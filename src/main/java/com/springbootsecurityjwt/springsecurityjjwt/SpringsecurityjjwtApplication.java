ee1730e5dc55c2136cc28b4ceecc478aa81a0498
Farisani-Ratshikombo <ratshikombofarisani@gmail.com>
2025-03-14 10:11:27 +0200

    adding spring initializer project

---
+++ src/main/java/com/springbootsecurityjwt/springsecurityjjwt/SpringsecurityjjwtApplication.java
@@ -0,0 +1,13 @@
+package com.springbootsecurityjwt.springsecurityjjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityjjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityjjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootsecurityjwt/springsecurityjjwt/SpringsecurityjjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootsecurityjwt.springsecurityjjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityjjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
